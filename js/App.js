58b911f7dc92537811324ffd07a0d77606b324d7
Erik <ericklo1094011@gmail.com>
2018-11-23 23:18:11 -0500

    Added code
    I added the constructor  for phrase and game files, for the app.js I added a new instance of the game class and I added some event listeners and some functions

---
+++ js/App.js
@@ -0,0 +1,30 @@
+/*-This file creates a new instance of the Game class, */
+game = new Game() 
+
+//-adds event listeners for the onscreen keyboard 
+// I used this piece of code as a referance: https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+document.getElementById('qwerty').addEventListener("click", function(){
+
+});
+
+/*Add an event listener to the "Start Game" button which calls the resetDisplay() function,
+ creates a new Game object, 
+ and starts the game. */
+document.getElementById('btn__reset').addEventListener("click", function(){
+    
+});
+
+
+//this function hides the start screen overlay.
+function resetDisplay() {
+    document.getElementById('overlay').style.display = 'hidden';
+}
+
+/*this function is called when a player selects a letter. 
+It disables the button on the onscreen keyboard 
+and calls the handleInteraction() method of the Game class.*/
+function markButton() {
+    
+}
+
+
