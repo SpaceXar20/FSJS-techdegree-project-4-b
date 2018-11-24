d9606a8290234ae7081af0932eef95bd828b389a
Erik <ericklo1094011@gmail.com>
2018-11-23 23:24:10 -0500

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,43 @@
+class Game {
+    constructor() {
+        this.missed = 0; // this will be used as a counter for the amount of tries a player will get
+        this.phrases = ["life is strange","success does not come easy", "seven swans", "guess the word", "wild goose chase"] 
+    }
+
+    //this method randomly retrieves one of the phrases stored in the phrases array.
+    getRandomPhrase() {
+
+    }
+
+    /*this method checks to see if the button clicked by the player matches a letter in the phrase.
+    If it does not, then call the removeLife() method..
+    If the selected letter matches, call the showMatchedLetter() method on the phrase and 
+    then call the checkForWin() method.
+    */
+    handleInteraction() {
+
+    }
+
+    /*this method removes a life, 
+    removes a heart from the board, 
+    and, if the player is out of lives, ends the game.*/
+    removeLife() {
+
+    }
+
+    //this method checks to see if the player has selected all of the letters.
+    checkForWin() {
+
+    }
+
+    //this method displays a message if the player wins or a different message if they lose.
+    gameOver() {
+
+    }
+
+    /* this method calls the getRandomPhrase() method,
+ and adds that phrase to the board by calling the Phrase class' addPhraseToDisplay() method. */
+ startGame() {
+     
+ }
+}
---
+++ js/Phrase.js
@@ -0,0 +1,21 @@
+class Phrase { //The class should include a constructor that accepts a phrase as an argument
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    //this method adds letter placeholders to the display when the game starts
+    addPhraseToDisplay() {
+
+    }
+
+    // this method checks to see if letter selected by player matches a letter in the phrase
+    checkLetter() {
+
+    }
+
+    // the method below reveals the letter(s) on the board that matches player's selection.
+    showMatchedLetter() {
+
+    }
+
+}
