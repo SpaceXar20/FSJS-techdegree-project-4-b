c7b49782b2d14ac91cd1c4b537b1e69f90d30ece
Erk L <ericklo1094011@gmail.com>
2018-12-15 23:10:05 -0500

    I added a keyboard compatibility
    I was able to make the keyboard be able to use to lay the game by adding regex in app.js

--- js/App.js
+++ js/App.js
@@ -11,14 +11,6 @@ const game = new Game()
     
  })
 
-//this works but this duplicates my phrases for some reason
-// $('#btn__reset').on('click',  (event) => {
-//     resetDisplay();
-//     game.startGame();
-// })
-
-
-
 
 //this function hides the start screen overlay.
 function resetDisplay() {
@@ -29,17 +21,35 @@ function resetDisplay() {
 /*this function is called when a player selects a letter.
 It disables the button on the onscreen keyboard
 and calls the handleInteraction() method of the Game class.*/
-function markButton(e) {
+function markButton(elem) {
+    console.log(elem) + ' (this key was pressed from the keyboard)';
      //I used a code snippet from this source: https://stackoverflow.com/questions/39340798/how-to-disable-submit-button-after-click-single-time
-    e.target.setAttribute('disabled', 'disabled');
-    e.target.classList.add('chosen');
-    game.handleInteraction(e.target.innerHTML);
+     elem.attr('disabled', 'disabled'); 
+     elem.addClass('chosen'); 
+     game.handleInteraction(elem.text()); 
 }
 
 //-adds event listeners for the onscreen keyboard
 // I used this piece of code as a referance: https://www.w3schools.com/jsref/met_element_addeventlistener.asp
 $('#qwerty button').on("click", function(e){
-    markButton(e);
+    markButton($(this));
+
     });
 
 game.startGame();
+
+//Allow player to use physical keyboard to guess letters, I used a snippet from here https://stackoverflow.com/questions/53798806/need-assistance-to-press-letters-from-keyboard?noredirect=1#comment94447687_53798806
+
+/*The keypress event makes it so that when you press a key it will get the element with the id equal to the letter in the html file,
+ and it will then run the markButton()*/
+$(document).keypress(function(e) { 
+    let key = String.fromCharCode(e.which); // e.which to get key code 
+    if (/[A-Za-z]/.test(key)) { // is the key a letter/character 
+    // If valid character, get the element key element using its id: 
+    markButton($("#"+key.toLowerCase())); 
+    } 
+    });
+
+
+    
+
